d923721036d2e8a523e1a16f903f1e268c66bb8a
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-25 15:32:21 +0000

    automatically backed up by learn

---
+++ lib/genre.rb
@@ -0,0 +1,20 @@
+class Genre
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def songs
+    Song.all.select{|song| song.genre == self}
+  end
+
+  def artists
+    self.songs.collect{|song| song.artist}
+  end
+end
